ee002400d1d42157269911aed6d64377d950b308
Nitu Dubey <nitutiwari01@gmail.com>
2023-07-12 22:15:42 -0400

    done

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,19 +1,16 @@
 import React from "react";
 
 const Child = (props) => {
-  
-
   return (
     <div className="child" style={{ backgroundColor: "#9C2121" }}>
       <h2>Child Component</h2>
       <button onClick={props.updateParent}>Show Modal</button>
       {props.modal == true ? (
         <div>
-          <h2>Modal Content</h2>
+          <h3>Modal Content</h3>
           <p>This is modal content</p>
         </div>
       ) : null}
-      
     </div>
   );
 };
